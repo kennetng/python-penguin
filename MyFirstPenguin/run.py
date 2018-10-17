c0fc33382572e61e7f74996969ee8605374bbe16
aswin <aswin@LAPTOP-69I8UF5E>
2018-10-17 20:30:02 +0200

    asdf

--- MyFirstPenguin/run.py
+++ MyFirstPenguin/run.py
@@ -19,7 +19,7 @@ MOVE_LEFT = {"top" : ROTATE_LEFT, "bottom" : ROTATE_RIGHT, "right" : ROTATE_RIGH
 
 
 def calcDistance(a, b):
-    return abs(a["x"] - b["x"]) + abs(a["y"] - b["y"])
+    return int(abs(a["x"] - b["x"]) + abs(a["y"] - b["y"]))
 
 def findPowerUp(body):
     
